977ecc5c13f89f195905b212cccc19aba480020a
Juan Velasco <jvelasco@iti.es>
2021-03-30 22:54:39 +0200

    Commit inicial

---
+++ pre/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_location" {
+  description = "Resource group location"
+  type        = string
+  default = "West Europe"
+}
+
+variable "vm_ssh_password" {
+  description = "Resource group location"
+  type        = string
+  default = "GeyPzmz56PALS4CxKnG"
+}
